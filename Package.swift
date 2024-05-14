b3df35fbcf2c2687f4efac319f12555c3fe73219
Dmitry Letko <letko.dmitry@gmail.com>
2024-05-14 15:48:26 +0200

    Update package dependency to make it compatible with updated SwiftLInt

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -19,7 +19,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "git@github.com:apple/swift-syntax.git", from: "509.0.0")
+        .package(url: "git@github.com:apple/swift-syntax.git", from: "510.0.2")
     ],
     targets: [
         .macro(
