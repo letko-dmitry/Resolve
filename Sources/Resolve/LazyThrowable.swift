30fd39071a38cf5772b48e7d1ee5e568decb4db7
Dmitry Letko <letko.dmitry@gmail.com>
2025-08-04 04:01:50 +0200

    Make lazy types more handy to use

--- Sources/Resolve/LazyThrowable.swift
+++ Sources/Resolve/LazyThrowable.swift
@@ -1,13 +1,13 @@
 //
-//  Lazy.swift
+//  LazyThrowable.swift
 //  Resolve
 //
-//  Created by Dzmitry Letko on 10/02/2025.
+//  Created by Dzmitry Letko on 04/08/2025.
 //
 
 import os.lock
 
-public final class Lazy<Value: Sendable>: Sendable {
+public final class LazyThrowable<Value: Sendable>: Sendable {
     private let state: OSAllocatedUnfairLock<State>
     private var resolution: Resolution {
         get async {
@@ -15,7 +15,7 @@ public final class Lazy<Value: Sendable>: Sendable {
                 switch current {
                 case .resolvable(let resolvable):
                     let task = Task {
-                        let value = await resolvable()
+                        let value = try await resolvable()
                         
                         state.withLock { state in
                             state = .resolved(value)
@@ -39,8 +39,8 @@ public final class Lazy<Value: Sendable>: Sendable {
     }
     
     public var value: Value {
-        get async {
-            await resolution.value
+        get async throws {
+            try await resolution.value
         }
     }
     
@@ -58,37 +58,37 @@ public final class Lazy<Value: Sendable>: Sendable {
         }
     }
     
-    public init(_ resolve: @Sendable @escaping () async -> Value) {
+    public init(_ resolve: @Sendable @escaping () async throws -> Value) {
         state = .init(initialState: .resolvable(resolve))
     }
     
     @inlinable
-    public func resolve() async -> Value {
-        await value
+    public func resolve() async throws -> Value {
+        try await value
     }
     
     @inlinable
-    public func callAsFunction() async -> Value {
-        await value
+    public func callAsFunction() async throws -> Value {
+        try await value
     }
 }
 
 // MARK: - private
-private extension Lazy {
+private extension LazyThrowable {
     enum State {
-        case resolvable(@Sendable () async -> Value)
-        case resolving(Task<Value, Never>)
+        case resolvable(@Sendable () async throws -> Value)
+        case resolving(Task<Value, any Error>)
         case resolved(Value)
     }
     
     enum Resolution {
         case value(Value)
-        case task(Task<Value, Never>)
+        case task(Task<Value, any Error>)
         
         var value: Value {
-            get async {
+            get async throws {
                 switch self {
-                case .task(let task): return await task.value
+                case .task(let task): return try await task.value
                 case .value(let value): return value
                 }
             }
